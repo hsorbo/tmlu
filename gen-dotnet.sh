1dbe9611b124b311b4fae89102b7e00ce8dd4643
Håvard Sørbø <havard@hsorbo.no>
2024-09-21 14:53:17 +0200

    gh action

--- gen-dotnet.sh
+++ gen-dotnet.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 set -e
+#dotnet tool install --global dotnet-xscgen --version 2.1.1162
 NS="Ariane.SaveFile.Contract"
 mkdir $NS
 pushd $NS
 dotnet new classlib
 rm -v *.cs
 xscgen -n "=$NS" ../tmlu.xsd
+dotnet pack /p:Version=0.0.1-preview1
